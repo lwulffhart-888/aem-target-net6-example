fd8fcf0c8dd0cdf979058b0b34061c55fb66e417
Liron Wulffhart <liron.wulffhart@888holdings.com>
2022-12-05 14:25:46 +0200

    Add project files.

---
+++ TargetWrapperLibNet21/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Adobe.Target.Client;
+
+namespace TargetWrapperLibNet21
+{
+    public static class Class1
+    {
+        public static IServiceCollection AddAEMTarget(this IServiceCollection services)
+        {
+            services.AddTargetLibrary();
+            return services;
+        }
+    }
+}
---
+++ TargetWrapperNet6/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.DependencyInjection;
+
+Console.WriteLine("Hello, World!");
+
+ServiceCollection services = new ServiceCollection();
+
+services.AddAEMTarget();
